e0bb51a060e811575fe545a76c32dd5cab7523c2
jnewman <jnewman@paperwise.com>
2019-01-26 12:18:50 -0600

    Playing

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,15 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/app_layout/material_persistent_drawer.dart';
+import 'package:angular_components/app_layout/material_temporary_drawer.dart';
 import 'package:angular_components/theme/dark_theme.dart';
 import 'package:angular_components/material_button/material_button.dart';
+import 'package:angular_components/material_button/material_fab.dart';
+import 'package:angular_components/focus/focus_item.dart';
+import 'package:angular_components/focus/focus_list.dart';
 import 'package:angular_components/material_icon/material_icon.dart';
+import 'package:angular_components/material_list/material_list.dart';
+import 'package:angular_components/material_list/material_list_item.dart';
+import 'package:angular_components/material_select/material_select_item.dart';
 import 'shit.dart';
 
 @Component(
@@ -9,15 +17,28 @@ import 'shit.dart';
   templateUrl: 'app_component.html',
   directives: const [ 
     DarkThemeDirective,
-    MaterialIconComponent, 
+    MaterialPersistentDrawerDirective,
+    MaterialTemporaryDrawerComponent,
     MaterialButtonComponent,
+    MaterialFabComponent,
+    FocusItemDirective,
+    FocusListDirective,
+    MaterialIconComponent,
+    MaterialListComponent,
+    MaterialListItemComponent,
+    MaterialSelectItemComponent,
     Shit
   ],
   styleUrls: const [
+    'package:angular_components/app_layout/layout.scss.css',
     'package:angular_components/css/mdc_web/card/mdc-card.scss.css',
     'app_component.scss.css'
   ]
 )
 class AppComponent {
   var name = 'Angular';
+
+  void selectItem(String item) {
+    print(item);
+  }
 }
